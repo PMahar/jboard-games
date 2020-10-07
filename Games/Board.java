0005760255d4077a1a2dd94f96396f3a7f121a1c
Jacob <jhausgaard>
2020-10-07 14:18:17 -0400

    Got row A working in the board

--- Games/Board.java
+++ Games/Board.java
@@ -2,8 +2,12 @@
 public class Board {
   Main main = new Main();
   public void drawBoard(){
+    for(int x = 0; x < 8; x++){
+     // This is just to test if the "board" works 
+      main.pieces.add("n");
+    }
     for( int i = 8; i > 0; i--){
-      System.out.println(i + " |" + main.pieces.get(i*1) +"| | | | | | | |");
+      System.out.println(i + " |" + main.pieces.get(i-1) +"| | | | | | | |");
     }
     System.out.println("   A B C D E F G H");
   }
