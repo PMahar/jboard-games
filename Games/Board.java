c43680102b495a45423172d9c83cc51e8055bebc
Jacob <jhausgaard>
2020-10-09 12:04:59 -0400

    Fixed my dumpster fire of code, Thanks to bdaroz

--- Games/Board.java
+++ Games/Board.java
@@ -1,14 +1,13 @@
-
 public class Board {
   Main main = new Main();
   // Matrix board will contain information on the location and types of pieces, if there are no pieces it 
-  // will show "n"
-  String[][] board = {"n","n","n","n","n","n","n","n"}, {"n","n","n","n","n","n","n","n"}, 
-  {"n","n","n","n","n","n","n","n"}, {"n","n","n","n","n","n","n","n"}, {"n","n","n","n","n","n","n","n"},
-  {"n","n","n","n","n","n","n","n"}, {"n","n","n","n","n","n","n","n"}, {"n","n","n","n","n","n","n","n"};
+  // will show "n"  
   public void drawBoard(){
-    for( int i = 8; i > 0; i--){
-      System.out.println(i + " |" + board[i - ][0] + "|"+ board[i - 1][1] + "|" + board[i - 1][2] + "|" + board[i - 1][3] + "|" + board[i - 1][4] + "|" + board[i - 1][5] + "|" + board[i - 1][6] + "|" + board[i - 1][7] + "|");
+    char[][] board = {{'n','n','n','n','n','n','n','n'},{'p','p','p','p','p','p','p','p'},
+  {'n','n','n','n','n','n','n','n'},{'n','n','n','n','n','n','n','n'},{'n','n','n','n','n','n','n','n'},
+      {'n','n','n','n','n','n','n','n'},{'p','p','p','p','p','p','p','p'},{'R','K','B','n','n','B','K','R'}};
+    for(int i = 8; i > 0; i--){
+      System.out.println(i + " |" + board[i - 1][0] + "|"+ board[i - 1][1] + "|" + board[i - 1][2] + "|" + board[i - 1][3] + "|" + board[i - 1][4] + "|" + board[i - 1][5] + "|" + board[i - 1][6] + "|" + board[i - 1][7] + "|");
     }
     System.out.println("   A B C D E F G H");
   }
--- Games/Main.java
+++ Games/Main.java
@@ -41,6 +41,7 @@ public class Main {
    * */
   public static void main(String[] args) {
     Board board = new Board();
+    System.out.println("Uppercase letters are white, lowercase are black");
     board.drawBoard();
   }
 } 
